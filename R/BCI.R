0f8fb32374fc434aadb9bdc7094804e90a1b7f5d
julianje <jjara@mit.edu>
2015-11-12 11:21:47 -0500

    Fixed correlation btsp

--- R/BCI.R
+++ R/BCI.R
@@ -12,25 +12,22 @@ TopCI <- function(data, R=10000){
 }
 
 CI_Corr <- function(d1,d2, R=10000){
-  data = rbind(mutate(d1,Type="d1"),mutate(d2,Type="d2"))
+  data = data.frame(d1,d2)
   samples <- boot::boot(data,function(x,id){
-    x$d1=sample(x$d1)
     samples<-x[id,]
     return(cor(samples$d1,samples$d2))},R)
   return(boot::boot.ci(samples,type="basic")$basic[c(4,5)])
 }
 LowerCI_Corr <- function(d1,d2, R=10000){
-  data = rbind(mutate(d1,Type="d1"),mutate(d2,Type="d2"))
+  data = data.frame(d1,d2)
   samples <- boot::boot(data,function(x,id){
-    x$d1=sample(x$d1)
     samples<-x[id,]
     return(cor(samples$d1,samples$d2))},R)
   return(boot::boot.ci(samples,type="basic")$basic[c(4)])
 }
 TopCI_Corr <- function(d1,d2, R=10000){
-  data = rbind(mutate(d1,Type="d1"),mutate(d2,Type="d2"))
+  data = data.frame(d1,d2)
   samples <- boot::boot(data,function(x,id){
-    x$d1=sample(x$d1)
     samples<-x[id,]
     return(cor(samples$d1,samples$d2))},R)
   return(boot::boot.ci(samples,type="basic")$basic[c(5)])
