266001580ff3bfa574481852e83b47dabacc7c2b
julianje <jjara@mit.edu>
2015-11-12 11:18:54 -0500

    Added correlation bootstrapps

--- R/BCI.R
+++ R/BCI.R
@@ -10,3 +10,28 @@ TopCI <- function(data, R=10000){
   samples <- boot::boot(data,function(x,id){return(mean(x[id]))},R)
   return(boot::boot.ci(samples,type="basic")$basic[c(5)])
 }
+
+CI_Corr <- function(d1,d2, R=10000){
+  data = rbind(mutate(d1,Type="d1"),mutate(d2,Type="d2"))
+  samples <- boot::boot(data,function(x,id){
+    x$d1=sample(x$d1)
+    samples<-x[id,]
+    return(cor(samples$d1,samples$d2))},R)
+  return(boot::boot.ci(samples,type="basic")$basic[c(4,5)])
+}
+LowerCI_Corr <- function(d1,d2, R=10000){
+  data = rbind(mutate(d1,Type="d1"),mutate(d2,Type="d2"))
+  samples <- boot::boot(data,function(x,id){
+    x$d1=sample(x$d1)
+    samples<-x[id,]
+    return(cor(samples$d1,samples$d2))},R)
+  return(boot::boot.ci(samples,type="basic")$basic[c(4)])
+}
+TopCI_Corr <- function(d1,d2, R=10000){
+  data = rbind(mutate(d1,Type="d1"),mutate(d2,Type="d2"))
+  samples <- boot::boot(data,function(x,id){
+    x$d1=sample(x$d1)
+    samples<-x[id,]
+    return(cor(samples$d1,samples$d2))},R)
+  return(boot::boot.ci(samples,type="basic")$basic[c(5)])
+}
